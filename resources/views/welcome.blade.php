55901c84e45dc990f838fd87293caa2ab024b9a7
robstar <199110+robstar@users.noreply.github.com>
2019-11-22 11:43:17 +0100

    added background switch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+		background-color: {{ config('flavour.backgroundColor', '#fff') }};
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
